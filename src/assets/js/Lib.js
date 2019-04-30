65d9394322d7bb3fc923c7324d8c8eef38cb3c88
xiaomaoaaa <94394302@qq.com>
2019-04-30 17:49:20 +0800

    第一次备份

---
+++ src/assets/js/Lib.js
@@ -0,0 +1,24 @@
+require('assets/css/reset.css');
+//解决click点击300毫秒延时问题
+// import FastClick from 'fastclick';
+// FastClick.attach(document.body);
+//自适应代码
+(function(doc, win) {
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function() {
+
+			var clientWidth = docEl.clientWidth;
+			if (!clientWidth) return;
+			if (clientWidth >= 750) {
+				docEl.style.fontSize = '100px';
+			} else {
+				docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+			}
+		};
+	if (!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+	/*DOMContentLoaded文档加载完成不包含图片资源 onload包含图片资源*/
+})(document, window);
